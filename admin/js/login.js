1ca3091be4ce6b1f915294792cf9ba6222a9fb94
codemaster-stack <tegaabinuwa@gmail.com>
2026-07-31 20:50:25 +0100

    login

---
+++ admin/js/login.js
@@ -0,0 +1,59 @@
+const API =
+"https://admission-api-r5y6.onrender.com/api/admin/login";
+
+const form = document.getElementById("loginForm");
+
+form.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+
+    const password = document.getElementById("password").value;
+
+    try {
+
+        const res = await fetch(API, {
+
+            method: "POST",
+
+            headers: {
+
+                "Content-Type": "application/json"
+
+            },
+
+            body: JSON.stringify({
+
+                email,
+                password
+
+            })
+
+        });
+
+        const data = await res.json();
+
+        if (!res.ok) {
+
+            document.getElementById("message").textContent =
+                data.message;
+
+            return;
+
+        }
+
+        localStorage.setItem("token", data.token);
+
+        window.location.href = "dashboard.html";
+
+    }
+
+    catch (err) {
+
+        document.getElementById("message").textContent =
+            "Network Error";
+
+    }
+
+});
