7b647e2a4b3e967c9aec811ae64488650316db82
codemaster-stack <tegaabinuwa@gmail.com>
2026-08-01 13:48:56 +0100

    fix

--- admin/js/dashboard.js
+++ admin/js/dashboard.js
@@ -128,7 +128,7 @@ async function loadDashboard() {
                     <td>${`${app.firstName} ${app.lastName}`}</td>
                     <td>${app.applicationStatus}</td>
                     <td>
-                        <a href="/application-details?id=${app._id}">
+                        <a href="/admin/application-details?id=${app._id}">
                             View
                         </a>
                     </td>
